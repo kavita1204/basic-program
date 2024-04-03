ed9300f036e206d07d6b6ef471f62820202b77a1
kavita1204 <kumarikavita7992@gmail.com>
2024-04-03 17:58:48 +0530

    first

---
+++ addtwonumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int a,b,c;
+    cout<<"int first number";
+    cin>>a;
+    cout<<"int second number";
+    cin>>b;
+    c=a+b;
+    cout<<"the sum is "<<c;
+    return 0;
+
+}
